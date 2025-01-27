5b27f685995f28419ff303b952488ec71b3d5c06
Jayandhan <jayandhanrajendran03@gmail.com>
2025-01-27 21:40:52 +0530

    Car file

--- Car.java
+++ Car.java
@@ -26,7 +26,6 @@ public class Main{
         System.out.println(NormalCar.maxSpeed);
         System.out.println(NormalCar.maxSpeed);
 
-
         RaceCar raceCar = new RaceCar();
         System.out.println(RaceCar.maxSpeed);
     }
